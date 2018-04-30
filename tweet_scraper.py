c16938ebe67af3ead1440f1b9c49c1d0ab2688c3
RespoTech <guillaume.foy.20160061@efrei.net>
2018-04-30 18:12:35 +0200

    Lots of changes and new things
    Tweets are now outputted by default in a mongo database(--database
    option removed). You can still see results by using the -test arg.
    Added --dbname option to name the db.
    
    Did some regexing to get get separately links and image links in the db.
    
    Did some error handling.
    
    Removed useless options --users and --tweets.
    
    Everything that needs to be (should be) is in french.

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -6,7 +6,6 @@ import aiohttp
 import asyncio
 import async_timeout
 import contextlib
-import csv
 import datetime
 import hashlib
 import json
@@ -16,7 +15,7 @@ import pymongo
 from pymongo import MongoClient
 import pdb #pour le debug à retirer
 
-## clean some output
+
 class RecycleObject(object):
     def write(self, junk): pass
 
@@ -33,22 +32,26 @@ def initdb(db):
         conn = client[db] #connecteur à la BDD
         return conn
     except:
-        print("\nCould not initialize database\n")
+        Error("Erreur 500", "Impossible d'initialiser la BDD.")
+
 
 
 
 async def getUrl(init):
     '''
-    URL Descision:
-    Tweep utilizes positions of Tweet's from Twitter's search feature to
-    iterate through a user's Twitter feed. This section decides whether
-    this is the first URL request or not and develops the URL based on the
-    args given.
+    Choix de l'URL
+    On utilise la position des Tweets dans le résultat de la recherche avancé de
+    Twitter pour itérer à travers le Tweets d'un utilisateur ou de la recherche.
 
-    Returns complete URL.
+    Cette section définit si c'est la première requete URL ou non, et crée un
+    URL selon les paramètres de recherche.
+
+    On fait une recherche sur Twitter en anglais "lang=en" pour que les timestamp des Tweets soient au format jj-mm-aa
+
+    Renvoie l'URL complet.
     '''
     if init == -1:
-        url = "https://twitter.com/search?f=tweets&vertical=default&lang=en&q="
+        url = "https://twitter.com/search?f=tweets&vertical=default&lang=en&q=" #première requête
     else:
         url = "https://twitter.com/i/search/timeline?f=tweets&vertical=default"
         url+= "&lang=en&include_available_features=1&include_entities=1&reset_"
@@ -82,7 +85,7 @@ async def getUrl(init):
 
 async def fetch(session, url):
     '''
-    Basic aiohttp request with a 30 second timeout.
+    Requête aiohttp avec une expiration de 30 secondes.
     '''
     with async_timeout.timeout(30):
         async with session.get(url) as response:
@@ -90,7 +93,7 @@ async def fetch(session, url):
 
 async def initial(response):
     '''
-    Initial response parsing and collecting the position ID
+    Parsing de la première réponse et récupération de l'ID de position.
     '''
     soup = BeautifulSoup(response, "html.parser")
     feed = soup.find_all("li", "js-stream-item")
@@ -100,7 +103,7 @@ async def initial(response):
 
 async def cont(response):
     '''
-    Regular json response parsing and collecting Position ID
+    Parsing de la réponse en json et récupération de l'ID de position.
     '''
     json_response = json.loads(response)
     html = json_response["items_html"]
@@ -114,13 +117,13 @@ async def cont(response):
 
 async def getFeed(init):
     '''
-    Parsing Descision:
+    Choix du type de parsing:
     Responses from requests with the position id's are JSON,
     so this section decides whether this is an initial request
     or not to use the approriate response reading for parsing
     with BeautifulSoup4.
 
-    Returns html for Tweets and position id.
+    Renvoie le html des Tweets et leur ID de position.
     '''
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
         response = await fetch(session, await getUrl(init))
@@ -131,47 +134,48 @@ async def getFeed(init):
         else:
             feed, init = await cont(response)
     except:
-        # Tweep will realize that it's done scraping.
         pass
 
     return feed, init
 
 async def outTweet(tweet):
-    '''
-    Parsing Section:
-    This function will create the desired output string and
-    write it to a file or csv if specified.
 
-    Returns output.
-    '''
     tweetid = tweet["data-item-id"]
-    # Formatting the date & time stamps just how I like it.
+
+    # On formate la date et le timestamp à la Française.
     datestamp = tweet.find("a", "tweet-timestamp")["title"].rpartition(" - ")[-1]
     d = datetime.datetime.strptime(datestamp, "%d %b %Y")
-    date = d.strftime("%Y-%m-%d")
+    date = d.strftime("%d-%m-%Y")
     timestamp = str(datetime.timedelta(seconds=int(tweet.find("span", "_timestamp")["data-time"]))).rpartition(", ")[-1]
     t = datetime.datetime.strptime(timestamp, "%H:%M:%S")
     time = t.strftime("%H:%M:%S")
-    # The @ in the username annoys me.
+
+    # On enlève le @ dans le nom d'utilisateur.
     username = tweet.find("span", "username").text.replace("@", "")
     timezone = strftime("%Z", gmtime())
-    # Replace all emoticons with their title, to be included in the tweet text
+
+    # On remplace tous les emoticons par leur nom pour qu'ils soient inclut dans le tweet.
     for img in tweet.findAll("img", "Emoji Emoji--forText"):
         img.replaceWith("<%s>" % img['aria-label'])
-    # The context of the Tweet compressed into a single line.
+
+    # On retourne tout le texte sur une ligne,un peu de mise en forme.
     text = tweet.find("p", "tweet-text").text.replace("\n", "").replace("http", " http").replace("pic.twitter", " pic.twitter")
-    # Regex for gathering hashtags
+
+    # Regex pour récupérer les hashtags.
     hashtags = ",".join(re.findall(r'(?i)\#\w+', text, flags=re.UNICODE))
+
+    # Regex pour récupérer les urls des images.
+    imglinks = ",".join(re.findall('pic.twitter.com/(?:[-\w.]|(?:%[\da-fA-F]{2}))+', text))
+
+    # Regex pour récupérer les urls.
+    links = ",".join(re.findall('https://(?:[-\w.]|(?:%[\da-fA-F]{2}))+|http://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', text))
+
+    # Recherche dans le html des tweets pour isolder le nombre de réponses, retweets et likes.
     replies = tweet.find("span", "ProfileTweet-action--reply u-hiddenVisually").find("span")["data-tweet-stat-count"]
     retweets = tweet.find("span", "ProfileTweet-action--retweet u-hiddenVisually").find("span")["data-tweet-stat-count"]
     likes = tweet.find("span", "ProfileTweet-action--favorite u-hiddenVisually").find("span")["data-tweet-stat-count"]
-    '''
-    This part tries to get a list of mentions.
-    It sometimes gets slow with Tweets that contain
-    40+ mentioned people.. rather than just appending
-    the whole list to the Tweet, it goes through each
-    one to make sure there arn't any duplicates.
-    '''
+
+
     try:
         mentions = tweet.find("div", "js-original-tweet")["data-mentions"].split(" ")
         for i in range(len(mentions)):
@@ -181,63 +185,52 @@ async def outTweet(tweet):
     except:
         pass
 
-    # Preparing to output
-
     '''
-    There were certain cases where I used Tweep
-    to gather a list of users and then fed that
-    generated list into Tweep. That's why these
-    modes exist.
+    Insertion des tweets/users et des métadonnées dans la BDD
     '''
-    if arg.database:
-        mydb.tweets.insert_one(
-        {"tweetid" : tweetid,
-        "date" : date,
-        "time" : time,
-        "timezone" : timezone,
-        "username" : username,
-        "text" : text,
-        "replies" : replies,
-        "likes" : likes,
-        "retweets" : retweets,
-        "hashtags" : hashtags
-        })
-
-    elif arg.users:
-        output = username
-    elif arg.tweets:
-        output = text
-    else:
-        '''
-        The standard output is how I like it, although
-        this can be modified to your desire. Uncomment
-        the bottom line and add in the variables in the
-        order you want them or how you want it to look.
-        '''
-        # output = ""
-        output = "{} {} {} {} <{}> {}".format(tweetid, date, time, timezone, username, text)
-        if arg.hashtags:
-            output+= " {}".format(hashtags)
-        if arg.stats:
-            output+= " | {} replies {} retweets {} likes".format(replies, retweets, likes)
+    mydb.tweets.insert_one(
+    {"tweetid" : tweetid,
+    "date" : date,
+    "time" : time,
+    "timezone" : timezone,
+    "username" : username,
+    "text" : text,
+    "replies" : replies,
+    "likes" : likes,
+    "retweets" : retweets,
+    "hashtags" : hashtags,
+    "imglinks" : imglinks,
+    "links" : links
+    })
 
 
-    return output
+
+    #On vérifie si l'utilisateur n'est pas déjà dans la BDD, sinon on l'ajoute.
+    if mydb.users.find({'username': username}).count() > 0:
+        mydb.users.insert_one({"username" : username})
+
+    if arg.test:
+        output = "\n{} {} {} {} <{}> {}".format(tweetid, date, time, timezone, username, text)
+        output+= " | {} replies {} retweets {} likes".format(replies, retweets, likes)
+        output += "\nImgLinks : {}".format(imglinks)
+        output += "\nLinks :{}".format(links)
+        output+= "\n {}".format(hashtags)
+        return output
+
 
 async def getTweets(init):
     '''
-    This function uses the html responses from getFeed()
-    and sends that info to the Tweet parser outTweet() and
-    outputs it.
+    Cette fonction utilise le html renvoyé par getFeed()
+    et envoie cette info au parser de Tweet et la renvoie.
 
-    Returns response feed, if it's first-run, and Tweet count.
+    Renvoie le file de réponse, si c'est la première itération, et le compte de Tweets.
     '''
     tweets, init = await getFeed(init)
     count = 0
     for tweet in tweets:
         '''
-        Certain Tweets get taken down for copyright but are still
-        visible in the search. We want to avoid those.
+        Certain Tweets sont supprimés pour des raisons de copyright mais sont
+        encore visible dans les résultats de recherche. On cherche à éviter ces Tweets.
         '''
         copyright = tweet.find("div","StreamItemContent--withheld")
         if copyright is None:
@@ -248,8 +241,7 @@ async def getTweets(init):
 
 async def getUsername():
     '''
-    This function uses a Twitter ID search to resolve a Twitter User
-    ID and return it's corresponding username.
+    Cette fonction permet d'obtenir le nom d'un utilisateur à partir d'un ID d'utilisateur.
     '''
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
         r = await fetch(session, "https://twitter.com/intent/user?user_id={0.userid}".format(arg))
@@ -257,13 +249,18 @@ async def getUsername():
     return soup.find("a", "fn url alternate-context")["href"].replace("/", "")
 
 async def main():
-    '''
-    Putting it all together.
-    '''
-    if arg.database:
-        print("Inserting into Database: " + str(arg.database))
-        global mydb
-        mydb = initdb(arg.database)
+    global mydb
+
+    if arg.dbname:
+        print("Insertion dans la BDD: " + str(arg.dbname))
+        mydb = initdb(arg.dbname)
+        if isinstance(mydb, str):
+            print(str)
+            sys.exit(1)
+
+    else:
+        print("Insertion dans la BDD: " + 'CONECT')
+        mydb = initdb('CONECT')
         if isinstance(mydb, str):
             print(str)
             sys.exit(1)
@@ -276,65 +273,51 @@ async def main():
     num = 0
     while True:
         '''
-        If our response from getFeed() has an exception,
-        it signifies there are no position IDs to continue
-        with, telling Tweep it's finished scraping.
+        Si la réponse de getFeed() à une exception,cela signifie qu'il n'y plus
+        d'IDs de position, donc le script a fini.
         '''
         if len(feed) > 0:
             feed, init, count = await getTweets(init)
             num += count
         else:
             break
-        # Control when we want to stop scraping.
         if arg.limit is not None and num >= int(arg.limit):
             break
 
-    if arg.database:
-        mydb.users.insert_one(
-        {"user" : str(arg.u),
-        "date_update" : str(datetime.datetime.now()),
-        "num_tweets" : num
-        })
 
     if arg.count:
-        print("Finished: Successfully collected {} Tweets.".format(num))
+        print("Fini: {} Tweets collectés.".format(num))
 
 def Error(error, message):
-    # Error formatting
     print("[-] {}: {}".format(error, message))
     sys.exit(0)
 
 def check():
-    # Performs main argument checks so nothing unintended happens.
     if arg.u is not None:
-        if arg.users:
-            Error("Contradicting Args", "Please use --users in combination with -s.")
         if arg.verified:
-            Error("Contradicting Args", "Please use --verified in combination with -s.")
+            Error("Arguments Contradictoires", "Utiliser --verified avec -s.")
         if arg.userid:
-            Error("Contradicting Args", "--userid and -u cannot be used together.")
-    if arg.tweets and arg.users:
-        Error("Contradicting Args", "--users and --tweets cannot be used together.")
+            Error("Arguments Contradictoires", "--userid et -u ne peuvent être utilisés ensemble.")
+    if int(arg.limit)%20 != 0:
+        Error("Argument Invalide", "La limite de Tweets scrapés doit être un multiple de 20")
+
 
 if __name__ == "__main__":
-    ap = argparse.ArgumentParser(prog="tweep.py", usage="python3 %(prog)s [options]", description="tweep.py - An Advanced Twitter Scraping Tool")
-    ap.add_argument("-u", help="User's Tweets you want to scrape.")
-    ap.add_argument("-s", help="Search for Tweets containing this word or phrase.")
-    ap.add_argument("-g", help="Search for geocoded tweets.")
-    ap.add_argument("-l", help="Search for Tweets in a certain language")
-    ap.add_argument("--year", help="Filter Tweets before specified year.")
-    ap.add_argument("--since", help="Filter Tweets sent since date (Example: 2017-12-27).")
-    ap.add_argument("--until", help="Filter Tweets sent until date (Example: 2017-12-27).")
-    ap.add_argument("--fruit", help="Display 'low-hanging-fruit' Tweets.", action="store_true")
-    ap.add_argument("--tweets", help="Display Tweets only.", action="store_true")
-    ap.add_argument("--verified", help="Display Tweets only from verified users (Use with -s).", action="store_true")
-    ap.add_argument("--users", help="Display users only (Use with -s).", action="store_true")
-    ap.add_argument("--hashtags", help="Output hashtags in seperate column.", action="store_true")
+    ap = argparse.ArgumentParser(prog="tweep.py", usage="python3 %(prog)s [options]", description="un scrapper de tweet adapté de tweep.py")
+    ap.add_argument("-u", help="Utilisateur dont on veut récupérer les Tweets.")
+    ap.add_argument("-s", help="Recherche des Tweets contenant ce mot ou cette phrase.")
+    ap.add_argument("-g", help="Recherche des Tweets géocodés.")
+    ap.add_argument("-l", help="Recherche des Tweets dans une langue")
+    ap.add_argument("-test", help="Argument a utilisé lorque l'on test le script dans le terminal")
+    ap.add_argument("--year", help="Filtrer les Tweets avant l'année spécifiée.")
+    ap.add_argument("--since", help="Filtrer les Tweets émis depuis la date spécifiée (Exemple: 2017-12-27).")
+    ap.add_argument("--until", help="Filtrer les Tweets émis jusqu'à la date spécifiée (Exemple: 2017-12-27).")
+    ap.add_argument("--fruit", help="Aficher les 'low-hanging-fruit' Tweets.", action="store_true")
+    ap.add_argument("--verified", help="Afficher seulement les Tweets d'utilisateurs verifiés (Utiliser avec -s).", action="store_true")
     ap.add_argument("--userid", help="Twitter user id")
-    ap.add_argument("--limit", help="Number of Tweets to pull (Increments of 20).")
-    ap.add_argument("--count", help="Display number Tweets scraped at the end of session.", action="store_true")
-    ap.add_argument("--stats", help="Show number of replies, retweets, and likes", action="store_true")
-    ap.add_argument("--database", help="Store tweets in Mongo database")
+    ap.add_argument("--limit", help="Limite le nombre de Tweets scrapés (Multiple de 20).")
+    ap.add_argument("--count", help="Affiche le nombre de Tweets scrapés.", action="store_true")
+    ap.add_argument("--dbname", help="Permet de spécifier le nom de la BDD (CONECT par défaut)")
     arg = ap.parse_args()
 
     check()
